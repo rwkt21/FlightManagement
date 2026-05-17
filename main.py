225562840f81c2da1681173696c5a51a0f246e42
rwkt21 <rwkt21@bath.ac.uk>
2026-05-17 08:18:45 +0000

    update comments

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sqlite3
 
 # Flight Management System
-# Manages flights, pilots, and destinations for an airline company
-# Uses SQLite3 for database operations via a command-line interface
+#-------------------------
+# Manages flights, pilots and destinations for an airline company
+# This uses SQLite3 for database operations via a command-line interface
+
 
 
 class DBOperations:
 
-    # SQL statement to create tables if they don't already exist
+    # SQL statement to create tables , it skips this step if the table already exists
     sql_create_pilot_table = """CREATE TABLE IF NOT EXISTS PILOT (
                                 pilot_id    INTEGER PRIMARY KEY,
                                 first_name  TEXT NOT NULL,
@@ -18,3 +20,4 @@ class DBOperations:
     sql_create_destination_table = """CREATE TABLE IF NOT EXISTS DESTINATION (
                                 airport_code TEXT PRIMARY KEY,
                                 airport_name TEXT NOT NULL,
+                                
